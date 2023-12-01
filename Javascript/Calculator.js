e86ea9fa72b5df6e053ece244c85787ecbe9c308
ieva <ieva.norbutaite@gmail.com>
2023-12-01 17:00:00 +0200

    Mantolandingpago pakeitimai

---
+++ Javascript/Calculator.js
@@ -0,0 +1,20 @@
+function Calculationrezult (){
+    let skaicius1 = document.querySelector("#skaicius1").value;
+    console.log(skaicius1);
+    let skaicius2 = document.querySelector("#skaicius2").value;
+    console.log(skaicius1);
+    let veiksmas = document.querySelector("#veiksmas").value;
+    //  if (skaicius1 && skaicius2 neivesti){
+    // console.log("Neivestas skaicius")} 
+    console.log(skaicius1);
+
+}
+    
+
+
+ 
+    
+
+ 
+
+ 
