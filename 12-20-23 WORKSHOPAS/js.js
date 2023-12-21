c7aaa61c5c772d9b45ff5ffce9f6bbb1770c23ba
ieva <ieva.norbutaite@gmail.com>
2023-12-21 02:37:37 +0200

    perziureta paskaitos medziaga ir super mini projektelio pradzia

---
+++ 12-20-23 WORKSHOPAS/js.js
@@ -0,0 +1,20 @@
+// // console.log("veikia");
+// const input = document.querySelector(".word");
+// const btn = document.querySelector(".btn");
+// const result = document.querySelector(".word");
+
+// btn.addEventListener("click", palindromas);
+ 
+// function palindromas (){
+//     const inputVal = input.value;
+//     console.log(inputVal)
+//     const len = inputVal.length;
+//     console.log(len);
+
+//     let start = inputVal.substring(0, Math.floor(len/2)).toLowerCase()
+//     let end = inputVal.substring(len-Math).floor()
+
+// let flip = end.split("")
+// // if (!/^[A-Za-z]+$/.test(inputValue))
+// }
+
