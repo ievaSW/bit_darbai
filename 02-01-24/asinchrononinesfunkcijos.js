342b311b60358111be12150a76eae8c4437f1b26
ieva <ieva.norbutaite@gmail.com>
2024-01-04 11:07:49 +0200

    kokteiliu aplikacija

--- 02-01-24/asinchrononinesfunkcijos.js
+++ 02-01-24/asinchrononinesfunkcijos.js
@@ -1,16 +1,50 @@
-const skaicius = 5;
-try {
-    skaicius++;
-} catch(error){
-    console.log(error);
-}
-console.log("Labas");
+// const skaicius = 5;
+// try {
+//     skaicius++;
+// } catch(error){
+//     console.log(error);
+// }
+// console.log("Labas");
 
-// Pažadas
-const pazadas = new Promise((resolve, reject)=>{
-    // prasidejo uzklausa i serveri"
-setTimeout(()=>resolve("zinute is serverio)", 1000);
-console.log("pasibaige uzklausa i serveri");)
-}).then((atsakymas)=>console.log(atsakymas))
+// // Pažadas
+// const pazadas = new Promise((resolve, reject)=>{
+//     // prasidejo uzklausa i serveri"
+// setTimeout(()=>resolve("zinute is serverio)", 1000);
+// console.log("pasibaige uzklausa i serveri");)
+// }).then((atsakymas)=>console.log(atsakymas))
+
+// const allUrls1 = [
+//     "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Vodka",
+// ],
+// allUrls2 = [
+//     "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin",
+//     "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Scotch",
+//     "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Triple_sec",
+//     "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Applejack",
+//     "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Vodka",
+// ],
+// allUrls3 = [
+//     "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin",
+//     "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Scotch",
+//     "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Triple_sec",
+//     "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Applejack",
+//     "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Brandy",
+//     "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Lemon_vodka",
+//     "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Champagne",
+//     "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Sherry",
+// ];
+
+// function fetchData()
+// {
+//     fetch(allUrls2[0])
+//     .then((response)=>response.json())
+//     .then((response)=>{console.log(response)
+//     const endTime = new Date();
+//     const duration = endTime.getTime() - startTime.getTime();
+//     console.log(duration + "ms");
+// });
+// }
+
+// fetchData();
 
 
--- coctails 12-28-23/js.js
+++ coctails 12-28-23/js.js
@@ -6,46 +6,78 @@ const  LuckyDrinkButton = document.getElementById("lucky")
 const  EnterDrinkInput = document.getElementById("Enter-drink")
 const  DrinkPhotodynamicHTML = document.getElementById("cocktails")
 
+// visu reiksmiu objektas
+
+const selectValues = {};
+
 const categoryArry = [];
 const glassArray = [];
 const ingredientsArry = [];
 
 const drinkArry =[];
 
+async function SelectElement(){
+    const allUrls = ["https://thecocktaildb.com/api/json/v1/1/list.php?c=list", 
+                     "https://thecocktaildb.com/api/json/v1/1/list.php?g=list",
+                     "https://thecocktaildb.com/api/json/v1/1/list.php?i=list"];
+
+
+    const allPromises = allUrls.map((url)=>fetch(url).then((response)=>response.json()));
+    console.log(allPromises);
+    const allValues = await Promise.all(allPromises);
+    // nerodo
+    console.log(allValues);
+
+    const [allCategories, allGlasses, allIngredients] = allValues;
+    selectValues.categories = allCategories.drinks.map((categoryObj)=>categoryObj.strCategory);
+    selectValues.glasses = allGlasses.drinks.map((glassesObj)=>glassesObj.strGlass);
+    selectValues.ingredients = allIngredients.drinks.map((ingredientsObj)=>ingredientsObj.strIngredient1);
+// console.log(selectValues.categories)
+    // console.log(selectValues);
+// pakeistos funkcijos, kurios uzpildo selecto laukelius
+    AllSelects(allCategories.drinks, DrinkCategorySelect, "strCategory")
+    AllSelects(allGlasses.drinks, DrinkGlassTypeSelect, "strGlass")
+    AllSelects(allIngredients.drinks, DrinkIngredientsSelect, "strIngredient1")
+
+}
+
+
+
+
 
 
 
 // 1. FETCHAI SELECTAMS
-async function SelectElements() {
-// Kokteiliu rusys
-await fetch("https://thecocktaildb.com/api/json/v1/1/list.php?c=list")
-.then((response) => response.json())
-// .then((response)=>console.log(response.drinks))
-.then((response) => {
+// async function SelectElements() {
+// // Kokteiliu rusys
+// await fetch("https://thecocktaildb.com/api/json/v1/1/list.php?c=list")
+// .then((response) => response.json())
+// // .then((response)=>console.log(response.drinks))
+// .then((response) => {
     
-AllSelects(response.drinks, DrinkCategorySelect, "strCategory")
-// map metodas grazina is objektu masyvo objektu laukus str. (Sudeda su masyvus?)
-categoryArry.push(...response.drinks.map((value)=>value.strCategory));
-})
+// AllSelects(response.drinks, DrinkCategorySelect, "strCategory")
+// // map metodas grazina is objektu masyvo objektu laukus str. (Sudeda su masyvus?)
+// categoryArry.push(...response.drinks.map((value)=>value.strCategory));
+// })
 
-.catch((error)=> console.log(error));
+// .catch((error)=> console.log(error));
 
-// Kokteiliu stikliniu tipai
-await fetch("http://thecocktaildb.com/api/json/v1/1/list.php?g=list")
-.then((response) => response.json())
-// .then((response)=>console.log(response.drinks))
-.then((response) => AllSelects(response.drinks, DrinkGlassTypeSelect, "strGlass"))
-.catch((error)=> console.log(error));
+// // Kokteiliu stikliniu tipai
+// await fetch("https://thecocktaildb.com/api/json/v1/1/list.php?g=list")
+// .then((response) => response.json())
+// // .then((response)=>console.log(response.drinks))
+// .then((response) => AllSelects(response.drinks, DrinkGlassTypeSelect, "strGlass"))
+// .catch((error)=> console.log(error));
 
-// Kokteiliu ingridientai
-await fetch("http://thecocktaildb.com/api/json/v1/1/list.php?i=list")
-.then((response) => response.json())
-.then((response) => AllSelects(response.drinks, DrinkIngredientsSelect, "strIngredient1"))
-// .then((response)=>console.log(response.drinks))
-.catch((error)=> console.log(error));
-}
+// // Kokteiliu ingridientai
+// await fetch("https://thecocktaildb.com/api/json/v1/1/list.php?i=list")
+// .then((response) => response.json())
+// .then((response) => AllSelects(response.drinks, DrinkIngredientsSelect, "strIngredient1"))
+// // .then((response)=>console.log(response.drinks))
+// .catch((error)=> console.log(error));
+// }
 
-
+// Funkcija, kuri užpildo selectus duomenimis iš API
 function AllSelects (DrinkGlassIngredients, SelectelementHTML, strDrinkGlassIngredient){
 let dynamicHTML = "";
 for (const DrinkGlassIngredient of DrinkGlassIngredients)
@@ -53,26 +85,34 @@ for (const DrinkGlassIngredient of DrinkGlassIngredients)
             dynamicHTML += 
             `<option>${DrinkGlassIngredient[strDrinkGlassIngredient]}</option>`;
         }
-        SelectelementHTML.innerHTML = dynamicHTML;
+        SelectelementHTML.innerHTML += dynamicHTML;
 }
 
 async function Alldrinks()
 {
     // https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary_Drink
-    for (const category of categoryArry){
+    const categoryDrinksUrls = [];
+    for (const category of selectValues.categories){
+
+
+        // console.log(selectValues.categories)
         let dynamicURL = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${category.replaceAll(" ", "_" )}`
-        const response = await fetch(dynamicURL);
-        const answerfromserver = await response.json();
-        for (const drink of answerfromserver.drinks)
-        {
-            drinkArry.push(drink)
-        }
+        // const response = await fetch(dynamicURL);
+        // const answerfromserver = await response.json();
+        // for (const drink of answerfromserver.drinks)
+        // {
+        //     drinkArry.push(drink)
+        // }
         // console.log(answerfromserver);
         // drinkArry.push(answerfromserver);
-        
+        const allPromises = categoryDrinksUrls.map((url)=>fetch(url).then((response)=>response.json()));
+        const allValues = await Promise.all(allPromises);
+        // paraso visu gerymu papusinima i drink arry
+        allValues.forEach((value)=>drinkArry.push(...value.drinks))
+        console.log(allValues);
     }
 }
-
+Alldrinks();
 function AlldrinksHTML(drinks){
 
     let dynamicHTML = "";
@@ -87,15 +127,50 @@ function AlldrinksHTML(drinks){
 }
 
 
+// FILTRACIJA
+
+
+
+async function filter()
+{
+    const EnterDrink = EnterDrinkInput.value,
+                  category =  DrinkCategorySelect.value,
+                glass =  DrinkGlassTypeSelect.value,
+                   ingredient =  DrinkIngredientsSelect.value;
+        // eiliskumas svarbu!!!
+        // pasitikrinimas ar veikia filtro funkcija
+        console.log(glass, category, ingredient, EnterDrink) ;       
+let filteredarry = [...drinkarry];
+
+// Paieska pagal pavadinima
+if(EnterDrink)
+{
+    filteredarry = filteredarry.filter((drinkObj)=>drinkObj.strDrink.toLowerCase().includes(EnterDrink))
+}
+
+if (category !== "Pasirinkite kategoriją"){
+    const promise = await fetch( `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${category.replaceAll(" ", "_" )}`);
+
+    const drinksofCategory = await promise.json();
+    console.log(drinksofCategory);
+    filteredArry = filteredArry.filter((drink)=> drinksofCategory.drinks.some(drinkOfCategory)=>drink.idDrink === drinkOfCategory.idDrink);
+}
+
+}
+
+
 async function initialization()
 {
     // selectu uzpildymas
-await SelectElements();
+    await SelectElement();
         await Alldrinks();
         // console.log(drinkArry);
     // gerimu atvaizdavimas
     AlldrinksHTML(drinkArry);
+    SearchDrinkButton.addEventListener("click", filter); 
 }
+// filter funkcija paduodama kaip parametras
+// SearchDrinkButton.addEventListener("click", filter);
 initialization();
 
 
